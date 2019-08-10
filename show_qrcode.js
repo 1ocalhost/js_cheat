63c2c90043000db00bcec643b4b1f668fb8e7da1
Pamela <aomeidev@gmail.com>
2019-08-11 01:17:36 +0800

    Support github.com
    the QR code image won't display on github.com due to their CSP.
    Now, there is another way to approach: open it in a new window.

--- show_qrcode.js
+++ show_qrcode.js
@@ -3,15 +3,19 @@
     return 'mmDotJsDotOrg_' + id;
   }
 
-  function findDiv(id) {
+  function findElement(id) {
     return document.getElementById(globalId(id));
   }
 
-  function createDiv(id, css) {
-    var div = document.createElement('div');
+  function createElement(tag, id, css) {
+    var div = document.createElement(tag);
     div.id = globalId(id);
     div.style.cssText = css;
     return div;
+  }
+
+  function createDiv(id, css) {
+    return createElement('div', id, css);
   }
 
   function initView() {
@@ -25,12 +29,11 @@
       right: 0;
       z-index: 2147483646;
     `);
-    mask.onclick = function() {
-      this.style.display = 'none';
-    };
     document.body.appendChild(mask);
 
-    let codeImg = createDiv('codeImg', `
+    let codeImgHost = createDiv('codeImgHost', `
+      padding: 10px;
+      box-sizing: border-box;
       position: fixed;
       width: 250px;
       height: 250px;
@@ -42,21 +45,55 @@
       border-radius: 2px;
       z-index: 2147483647;
     `);
-    mask.appendChild(codeImg);
+
+    tip = createElement('a', 'tip', `
+      color: #ef64a3;
+      font-size: 16px;
+      cursor: pointer;
+    `);
+    tip.text = 'open image in new window';
+
+    let codeImg = createDiv('codeImg', `
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      pointer-events: none;
+    `);
+
+    codeImgHost.appendChild(tip);
+    codeImgHost.appendChild(codeImg);
+    mask.appendChild(codeImgHost);
+  }
+
+  function qrCodeUri() {
+    return 'https://chart.googleapis.com/chart?chs=250x250&cht=qr&choe=UTF-8&chl='
+      + window.encodeURIComponent(window.location.href);
   }
 
   function showCodeImg() {
-    let qrCodeUrl = "https://chart.googleapis.com/chart?chs=250x250&cht=qr&choe=UTF-8&chl="
-      + window.encodeURIComponent(window.location.href);
+    let subWindow = null;
+    findElement('tip').onclick = () => {
+      event.stopPropagation();
+      subWindow = window.open(qrCodeUri(), 'subWindow', 'width=250,height=247');
+    };
 
-    let mask = findDiv('mask');
-    let codeImg = findDiv('codeImg');
+    let mask = findElement('mask');
+    mask.onclick = function() {
+      this.style.display = 'none';
+      if (subWindow) {
+        subWindow.close();
+      }
+    };
+
+    let codeImg = findElement('codeImg');
     codeImg.title = window.location.href;
-    codeImg.style.backgroundImage = 'url(' + qrCodeUrl + ')';
+    codeImg.style.backgroundImage = `url(${qrCodeUri()})`;
     mask.style.display = 'block';
   }
 
-  if (!findDiv('mask')) {
+  if (!findElement('mask')) {
     initView();
   }
 
